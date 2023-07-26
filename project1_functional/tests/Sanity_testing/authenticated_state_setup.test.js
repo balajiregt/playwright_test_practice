846b7154d9b0f8050ba13d1719c8d0f19aec01d6
kbalaji <balaji.regt@gmail.com>
2023-07-26 21:10:04 +0530

    browser state authentication steps

---
+++ project1_functional/tests/Sanity_testing/authenticated_state_setup.test.js
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+const authFile = 'C:/Users/kbala/OneDrive/Desktop/Playwright_projects/project1_functional/tests/Sanity_testing/auth.json';
+
+test('authenticate', async ({ page }) => {
+  // Perform authentication steps. Replace these actions with your own.
+  await page.goto('https://bookcart.azurewebsites.net/login');
+  await page.locator('#mat-input-0').fill('waheedahiya');
+  await page.locator('#mat-input-1').fill('Pass123#');
+  await page.locator('.mat-focus-indicator.mat-raised-button.mat-button-base.mat-primary').click();
+  
+  // Wait until the page receives the cookies.
+  await page.waitForTimeout(10000)
+
+  // End of authentication steps-save the session states in the json file
+  await page.context().storageState({ path: authFile });
+})
+
+
